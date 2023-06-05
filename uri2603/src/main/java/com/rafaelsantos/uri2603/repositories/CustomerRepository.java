6e7f38564db4a918d3dc05bf0cf7c2615ddfec5a
Rafael Santos <rafa.05125@gmail.com>
2023-06-05 20:13:09 -0300

    JPQL query

--- uri2603/src/main/java/com/rafaelsantos/uri2603/repositories/CustomerRepository.java
+++ uri2603/src/main/java/com/rafaelsantos/uri2603/repositories/CustomerRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.rafaelsantos.uri2603.dto.CustomerMinDTO;
 import com.rafaelsantos.uri2603.entities.Customer;
 import com.rafaelsantos.uri2603.projections.CustomerMinProjection;
 
@@ -14,4 +15,9 @@ public interface CustomerRepository extends JpaRepository<Customer, Long>{
 			+ "FROM customers "
 			+ "WHERE city= :city")
 	List<CustomerMinProjection> sqlSearch(String city);
+	
+	@Query("SELECT new com.rafaelsantos.uri2603.dto.CustomerMinDTO(obj.name, obj.street) "
+			+ "FROM Customer obj "
+			+ "WHERE obj.city= :city")
+	List<CustomerMinDTO> jpqlSearch(String city);
 }
